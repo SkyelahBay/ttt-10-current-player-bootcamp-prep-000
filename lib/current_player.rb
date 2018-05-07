23a7d7f7fb6aa0d2179d68ec89f73a7d08af9ec8
Skyelah Hanna Bay <skyelahbay@gmail.com>
2018-05-07 17:46:53 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count(board)
   count = 0
   board.each do |space|
-    if board[space] == "X" || board[space] == "O"
+    if space == "X" || space == "O"
       count += 1
     end #end if
   end #end .each
